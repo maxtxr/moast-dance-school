48537d228be0efef2a5e5ac94ecf9c820af8fb9f
Bruna Rossa <115885004+maxtxr@users.noreply.github.com>
2026-05-19 19:31:09 +0100

    Revert "Delete src/data directory"
    This reverts commit 9fbed5cf0f3fb4a58d74b6911f862a2417dab4d5.

---
+++ src/data/merch.js
@@ -0,0 +1,15 @@
+export const merchItems = [
+    { name: 'KeyChain - Porta-chaves', details: '', price: '2,50€' },
+    { name: 'Rhythm Feet - Meias', details: '(brancas e pretas)', price: '3,50€' },
+    { name: 'Strap Panama - Panamá com fita', details: '(bege, rosa e preto)', price: '5€' },
+    { name: 'Suede Panama - Panamá de veludo', details: '(bege e preto)', price: '6€' },
+    { name: 'Sway Tote - Saco de Pano', details: '(bege e preta)', price: '7€' },
+    { name: 'Groove Tee - T-shirt', details: '(branca ou preta)', price: '12€' },
+    { name: 'Flow Fit - Camisola Justa', details: '(branca, preta ou roxa)', price: '12€' },
+    { name: 'Hoodie - Sweatshirt', details: '(branca ou preta)', price: '27,50€' },
+    {
+        name: 'Kit Garrafa Térmica 1,1L',
+        details: 'Garrafa + Palhinha de Aço Inox + Escovilhão\n(branca + personalização de nome)',
+        price: '25€'
+    },
+];
